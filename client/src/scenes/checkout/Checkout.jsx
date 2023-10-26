641c74dd7ca5e77ccf18261b3f75603581b6ca4f
Mirza Arafat Baig <103023081+arafatbaig@users.noreply.github.com>
2023-10-26 13:10:20 +0530

    Update Checkout.jsx

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -47,7 +47,7 @@ const Checkout = () => {
       })),
     };
 
-    const response = await fetch("http://localhost:1337/api/orders", {
+    const response = await fetch("https://clothing-site-psi.vercel.app/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(requestBody),
