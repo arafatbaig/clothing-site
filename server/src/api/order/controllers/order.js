44444ce033f43de5657593c64d65b768f717f9b6
Mirza Arafat Baig <103023081+arafatbaig@users.noreply.github.com>
2023-10-26 13:15:55 +0530

    Update order.js

--- server/src/api/order/controllers/order.js
+++ server/src/api/order/controllers/order.js
@@ -38,8 +38,8 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
         payment_method_types: ["card"],
         customer_email: email,
         mode: "payment",
-        success_url: "http://localhost:3000/checkout/success",
-        cancel_url: "http://localhost:3000",
+        success_url: "https://clothing-site-2kny.vercel.app//checkout/success",
+        cancel_url: "https://clothing-site-2kny.vercel.app/",
         line_items: lineItems,
       });
 
